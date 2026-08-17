ef456eff3ec2cd5bb5bf01e3ecac9031b386315f
fahriz aziz <azizfahriz@gmail.com>
2026-08-17 11:00:37 +0700

    chore: init boilerplate and modular monolith structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { SyncModule } from './sync/sync.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProductsModule, OrdersModule, SyncModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
